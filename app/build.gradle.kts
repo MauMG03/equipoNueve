d323a636bdc1260157725176d670ef83436431b3
MauMG03 <mauricio.munoz.gutierrez@correounivalle.edu.co>
2024-05-30 22:11:02 -0500

    Project structure created

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -40,12 +41,56 @@ android {
 }
 
 dependencies {
+    val navVersion = "2.3.5"
 
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //Navigation
+    implementation ("androidx.navigation:navigation-fragment-ktx:$navVersion")
+    implementation ("androidx.navigation:navigation-ui-ktx:$navVersion")
+    implementation("androidx.navigation:navigation-common:$navVersion")
+
+    //CardView
+    implementation("androidx.cardview:cardview:1.0.0")
+
+    //RecyclerView
+    implementation("androidx.recyclerview:recyclerview:1.3.1")
+
+    //Corrutinas
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+
+    //ViewModel
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    implementation ("androidx.activity:activity-ktx:1.8.0")
+    implementation ("androidx.fragment:fragment-ktx:1.6.2")
+
+    // LiveData
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.3.1")
+
+    // Room
+    implementation ("androidx.room:room-runtime:2.5.2")
+    implementation ("androidx.room:room-ktx:2.5.2")
+    ksp("androidx.room:room-compiler:2.5.2")
+
+    //Boton flotante
+    implementation ("com.getbase:floatingactionbutton:1.10.1")
+
+    //Retrofit (Consumo de API's)
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    //Glide (Consumir imagenes API)
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
+
+    //Toolbar
+    implementation("androidx.coordinatorlayout:coordinatorlayout:1.2.0")
+    implementation("androidx.drawerlayout:drawerlayout:1.2.0")
 }
--- app/src/main/java/com/example/miniproyecto2/view/MainActivity.kt
+++ app/src/main/java/com/example/miniproyecto2/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.miniproyecto2
+package com.example.miniproyecto2.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.miniproyecto2.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
