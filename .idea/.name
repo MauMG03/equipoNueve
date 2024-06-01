c4364e6f8f7a916f678ec85313536b498f486cb2
TheCryss <38475424+TheCryss@users.noreply.github.com>
2024-05-31 23:48:34 -0500

    feat(login): UI created for the login page

---
+++ .idea/.name
@@ -0,0 +1,1 @@
+miniproyecto2
--- app/src/main/java/com/example/miniproyecto2/view/fragment/LoginFragment.kt
+++ app/src/main/java/com/example/miniproyecto2/view/fragment/LoginFragment.kt
@@ -6,26 +6,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.miniproyecto2.R
+import com.example.miniproyecto2.databinding.FragmentLoginBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class LoginFragment : Fragment() {
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-
-        }
-    }
+    private lateinit var binding: FragmentLoginBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        //return inflater.inflate(R.layout.fragment_login, container, false)
+        binding = FragmentLoginBinding.inflate(inflater)
+        binding.lifecycleOwner = this
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+
 
 
 }
