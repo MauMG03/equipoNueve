cbdfb3bd87967b4ed2083fed508dd0a0c706c811
GetialJuan <getial.juan@correounivalle.edu.co>
2024-06-01 15:14:51 -0500

    feat(Home): FireStore items
    Added:
    - Item model
    - Item dao
    - Item repository
    - Item viewModel
    - Item recycler view

--- app/src/main/java/com/example/miniproyecto2/di/Module.kt
+++ app/src/main/java/com/example/miniproyecto2/di/Module.kt
@@ -1,8 +1,12 @@
 package com.example.miniproyecto2.di
 
+import android.content.Context
+import com.example.miniproyecto2.data.InventoryDao
+import com.google.firebase.firestore.FirebaseFirestore
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -19,5 +23,11 @@ object Module {
             .baseUrl("Here comes something diferent")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideFirestoreDB(): FirebaseFirestore{
+        return FirebaseFirestore.getInstance()
     }
 }
---
+++ app/src/main/java/com/example/miniproyecto2/repository/InventoryRepository.kt
@@ -0,0 +1,18 @@
+package com.example.miniproyecto2.repository
+
+import com.example.miniproyecto2.data.InventoryDao
+import com.example.miniproyecto2.model.Item
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class InventoryRepository @Inject constructor(
+    private val inventoryDao: InventoryDao
+) {
+    suspend fun getItems():MutableList<Item>{
+        return withContext(Dispatchers.IO){
+            inventoryDao.getItems()
+        }
+    }
+}
