0b45058778a20543e80a1fbbd0582f809e4f6782
MauMG03 <mauricio.munoz.gutierrez@correounivalle.edu.co>
2024-06-02 19:33:07 -0500

    added some config with the api service

--- app/src/main/java/com/example/miniproyecto2/webservice/ApiService.kt
+++ app/src/main/java/com/example/miniproyecto2/webservice/ApiService.kt
@@ -1,8 +1,12 @@
 package com.example.miniproyecto2.webservice
 
+import com.example.miniproyecto2.model.ImageResponse
 import com.example.miniproyecto2.model.Item
+import com.example.miniproyecto2.utils.Constants.ENDPOINT_IMAGE
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface ApiService {
-
+    @GET(ENDPOINT_IMAGE)
+    suspend fun getImage(@Path("key") key:String, @Path("query") query:String): ImageResponse
 }
