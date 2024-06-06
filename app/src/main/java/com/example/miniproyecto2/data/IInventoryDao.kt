b970a6ab08c5f6cb39d2669311a77e6d9a82cbcd
Juan Sebastian Getial Getial <99220926+GetialJuan@users.noreply.github.com>
2024-06-05 21:02:22 -0500

    Revert "Home"

--- app/src/main/java/com/example/miniproyecto2/data/IInventoryDao.kt
+++
@@ -1,7 +0,0 @@
-package com.example.miniproyecto2.data
-
-import com.example.miniproyecto2.model.Item
-
-interface IInventoryDao {
-    suspend fun getItems(): MutableList<Item>
-}
--- app/src/main/java/com/example/miniproyecto2/di/Module.kt
+++ app/src/main/java/com/example/miniproyecto2/di/Module.kt
@@ -1,12 +1,8 @@
 package com.example.miniproyecto2.di
 
-import android.content.Context
-import com.example.miniproyecto2.data.InventoryDao
-import com.google.firebase.firestore.FirebaseFirestore
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -23,11 +19,5 @@ object Module {
             .baseUrl("Here comes something diferent")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
-    }
-
-    @Singleton
-    @Provides
-    fun provideFirestoreDB(): FirebaseFirestore{
-        return FirebaseFirestore.getInstance()
     }
 }
