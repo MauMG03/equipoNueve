1266ac55acb87202a5bced9aad1123ce6d8db76e
MauMG03 <mauricio.munoz.gutierrez@correounivalle.edu.co>
2024-06-07 17:21:59 -0500

    Added some tests

--- app/src/test/java/com/example/miniproyecto2/viewmodel/InventoryViewModelTest.kt
+++ app/src/test/java/com/example/miniproyecto2/viewmodel/InventoryViewModelTest.kt
@@ -34,7 +34,7 @@ class InventoryViewModelTest {
         Dispatchers.setMain(UnconfinedTestDispatcher())
         val mockItems = mutableListOf(
             Item(
-                "Cargador", "Cargador de cel", 15000.0,
+                "1", "Cargador", "Cargador de cel", 15000.0,
                 3, "image", "Tecnologia")
         )
         Mockito.`when`(inventoryRepository.getItems()).thenReturn(mockItems)
@@ -44,5 +44,71 @@ class InventoryViewModelTest {
 
         //then
         assertEquals(inventoryViewModel.items.value, mockItems)
+    }
+
+    @Test
+    fun `test editItem method`() = runBlocking {
+        // Given
+        Dispatchers.setMain(UnconfinedTestDispatcher())
+        val id = "1"
+        val name = "Test Item"
+        val description = "Test Description"
+        val price = 100.0
+        val quantity = 10
+        val category = "Test Category"
+        val image = "Test Image"
+
+        val item = Item(id, name, description, price, quantity, image, category)
+
+        Mockito.`when`(imageResponseRepository.getImage(name)).thenReturn(image)
+
+        // When
+        inventoryViewModel.editItem(id, name, description, price, quantity, category)
+
+        // Then
+        Mockito.verify(inventoryRepository).editItem(item)
+    }
+
+    @Test
+    fun `test metodo addItem`() = runBlocking{
+        //given
+        Dispatchers.setMain(UnconfinedTestDispatcher())
+        val id = ""
+        val name = "Sofa con un nombre muy extrano"
+        val image = "imagen random"
+        val des = "Mueble muy comodo"
+        val price = 15000.0
+        val quantity = 3
+        val category = "Muebles"
+
+        Mockito.`when`(imageResponseRepository.getImage(name)).thenReturn(image)
+
+        //when
+        val item = Item(id,name,des,price,quantity,image,category)
+        inventoryViewModel.addItem(name,des,price,quantity,category)
+
+        //verify
+        Mockito.verify(inventoryRepository).addItem(item)
+    }
+
+    @Test
+    fun `test metodo deleteItem`() = runBlocking{
+        //given
+        Dispatchers.setMain(UnconfinedTestDispatcher())
+        val id = ""
+        val name = "Sofa con un nombre muy extrano"
+        val image = "imagen random"
+        val des = "Mueble muy comodo"
+        val price = 15000.0
+        val quantity = 3
+        val category = "Muebles"
+
+        val item = Item(id,name,des,price,quantity,image,category)
+
+        //when
+        inventoryViewModel.deleteItem(item)
+
+        //verify
+        Mockito.verify(inventoryRepository).deleteItem(item)
     }
 }
